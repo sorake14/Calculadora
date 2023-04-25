97c4173cf8501540b5a23f3157d2507101373e90
Paulo <paulocesar147852@gmail.com>
2023-04-25 20:24:29 -0300

    ajuste inputs

--- script.js
+++ script.js
@@ -17,6 +17,10 @@ header.style.flexDirection = "column"
 document.body.appendChild(header)
 //FIM DO HEADER
 
+
+
+
+
 //LOGO DO HEADER
 const logo = document.createElement("img")
 logo.src = "imgs/logo.png"
@@ -27,7 +31,12 @@ logo.style.width = "100px"
 header.appendChild(logo)
 //FIM DO LOGO DO HEADER
 
-//Titulo do Header
+
+
+
+
+
+//TITULO DO HEADER
 
 const titulo = document.createElement("h1")
 
@@ -35,6 +44,11 @@ titulo.textContent = "Calculadora IMC"
 
 header.appendChild(titulo)
 //FIM DO TITULO DO HEADER
+
+
+
+
+
 
 //BOX DO BODY
 
@@ -55,39 +69,91 @@ const box2 = document.createElement("div")
 box2.style.height = "80%"
 box2.style.width = "700px"
 box2.style.backgroundColor = "#b5b5b5"
-box2.style.border = "11px double gray"
+box2.style.border = "11px dashed gray"
 box2.style.display = "flex"
+box2.style.justifyContent = "start"
+box2.style.alignItems = "center"
+box2.style.flexDirection = "column"
+box2.style.padding = "20px"
 
 
 box1.appendChild(box2)
 //FIM DA BOX DO BODY
 
-//NOME PESO E ALTURA
 
-const nome = document.createElement("input")
-nome.placeholder = "Digite seu nome"
-nome.style.height = "30px"
-nome.style.width = "50px"
 
-box2.appendChild(nome)
 
+
+
+
+
+//NOME, PESO, ALTURA E RESULTADO
+
+const stringnome = document.createElement("h3")
+stringnome.textContent = "Nome:"
 box2.appendChild(stringnome)
 
-const peso = document.createElement("input")
-peso.style.height = "30px"
-peso.style.width = "50px"
+const nome = document.createElement("input")
+nome.id = "nome"
+nome.type = "text"
+nome.placeholder = "Digite seu nome"
+nome.style.textAlign = "center"
+nome.style.height = "30px"
+nome.style.width = "500px"
+box2.appendChild(nome)
 
+
+
+const stringpeso = document.createElement("h3")
+stringpeso.textContent = "Peso:"
+box2.appendChild(stringpeso)
+
+const peso = document.createElement("input")
+peso.id = "peso"
+peso.type = "number"
+peso.style.textAlign = "center"
+peso.style.height = "30px"
+peso.style.width = "100px"
 box2.appendChild(peso)
 
+
+
+const stringaltura = document.createElement("h3")
+stringaltura.textContent = "Altura:"
+box2.appendChild(stringaltura)
+
 const altura = document.createElement("input")
+altura.id = "altura"
+altura.type = "number"
+altura.step = "1.01"
+altura.style.textAlign = "center"
 altura.style.height = "30px"
-altura.style.width = "50px"
+altura.style.width = "100px"
 
 box2.appendChild(altura)
 
-//FIM DO NOME PESO E ALTURA
 
 
+
+//FIM DO NOME, PESO, ALTURA E RESULTADO
+
+
+//BOTAO CALCULAR
+const botaocalcular = document.createElement("button")
+botaocalcular.textContent = "Calcular"
+botaocalcular.style.height = "30px"
+botaocalcular.style.width = "100px"
+botaocalcular.style.marginTop = "20px"
+botaocalcular.style.marginBottom = "20px"
+
+box2.appendChild(botaocalcular)
+
+botaocalcular.addEventListener("click", function(){
+    const resultado = peso / (altura * altura)
+})
+
+
+//FIM DO BOTAO CALCULAR
 
 
 
